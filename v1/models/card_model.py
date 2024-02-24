036f28b5248b4fdd7a4d57f0226c365289922241
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-02-24 13:30:12 -0600

    Make changes to the application absed on the needs of the MVP after consulting with the front end team

--- v1/models/card_model.py
+++ v1/models/card_model.py
@@ -1,13 +1,12 @@
 from django.db import models
 
-# card class is NOT migrated yet
 class Card(models.Model):
   name = models.CharField(max_length=255)
   mana_cost = models.CharField(max_length=50)
   cmc = models.IntegerField(default=0)
-  # colors = ??? it's an array. May need to set up a POPO to save it as a string.
+  colors = models.CharField(max_length=100, default='')
   type_line = models.CharField(max_length=255)
-  # set = models.CharField(max_length=255)
+  # set = models.CharField(max_length=255) # For a future iteration
   rarity = models.CharField(max_length=50)
   cmdr_legal = models.CharField(max_length=50)
   img = models.URLField()
--- v1/views/card_views.py
+++ v1/views/card_views.py
@@ -6,23 +6,3 @@ from ..services.card_service import CardService
 # from ..serializers import CardSerializer
 from ..models.card_model import Card
 
-def find_cards(request):
-  query = request.GET.get('query')
-  if query:
-    cards_data = CardService.search_cards(query)
-    for details in cards_data:
-      card = Card.objects.create(
-        name=details.get('name'),
-        mana_cost=details.get('mana_cost'),
-        cmc=details.get('cmc'),
-        # color=details.get('color'),
-        type_line=details.get('type_line'),
-        rarity=details.get('rarity'),
-        cmdr_legal=details.get('legalities').get('commander'),
-        img=details.get('image_uris').get('normal'),
-        purchase_uris=details.get('purchase_uris').get('tcgplayer')
-      )
-      card.save()
-    return JsonResponse({'message': 'Card populated successfully'})
-  else:
-    return JsonResponse({'error': 'Query parameter is required'}, status=400)
