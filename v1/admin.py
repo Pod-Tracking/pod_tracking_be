ee3a8f7194f9d8234ec4c43e6abd05f5aa3e2d74
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-02-26 16:07:50 -0600

    Create the url paths to access complete CRUD functionality for all models, and update the admin.py and requirements.txt files

--- v1/admin.py
+++ v1/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models.card_model import Card
+from .models.commander_model import Commander
+from .models.player_model import Player
+from .models.deck_model import Deck
 
-admin.site.register(Card)
+admin.site.register(Player)
+admin.site.register(Commander)
+admin.site.register(Deck)
 
 # Register your models here.
