7415481e30b0cb3b6a66950b41e26d97685feaa8
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-02-26 16:05:43 -0600

    Create the commander_serializer.py file to format the json response of commander objects

---
+++ v1/serializers/commander_serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from ..models.commander_model import Commander
+import pdb
+
+class CommanderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Commander
+        fields = ['name', 'colors', 'img']
