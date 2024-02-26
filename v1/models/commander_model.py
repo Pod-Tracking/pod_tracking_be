469dfd2da92a44d21aefdedc5206dbc5169ad0dd
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-02-26 15:59:07 -0600

    Remove the initial card model that served as a prototype and replace it with the more appropriate, commander model

---
+++ v1/models/commander_model.py
@@ -0,0 +1,12 @@
+from django.db import models
+from ..models.deck_model import Deck
+
+class Commander(models.Model):
+    deck = models.OneToOneField(Deck, on_delete=models.CASCADE, null=False, default=None)
+    name = models.CharField(max_length=255)
+    colors = models.CharField(max_length=100, default='')
+    img = models.URLField()
+
+    def __str__(self):
+        return self.name
+    
