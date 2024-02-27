4791ae60a989bca92da4059d9cac7d675fc6b6eb
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-02-27 15:24:43 -0600

    Add the updated and created date fields to the deck model for filtering and organization on the front end later

--- v1/models/deck_model.py
+++ v1/models/deck_model.py
@@ -1,12 +1,15 @@
 from django.db import models
 from ..models.player_model import Player
+from django.utils import timezone
 
 class Deck(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, null=False)
     player = models.ForeignKey(Player, on_delete=models.CASCADE, null=False)
     colors = models.CharField(max_length=100, default='')
     wins = models.IntegerField(default=0)
     losses = models.IntegerField(default=0)
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
