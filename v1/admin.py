b7ad58646a156709b6a226fee06dac56e8a26623
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-03-03 00:55:44 -0600

    Update the basic file functions to enable the automation of commander colors to deck colors

--- v1/admin.py
+++ v1/admin.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 from .models.commander_model import Commander
 from .models.player_model import Player
 from .models.deck_model import Deck
+from .models.pod_model import Pod
+from .models.pod_player_model import PodPlayer
+from .models.game_model import Game
 
 admin.site.register(Player)
 admin.site.register(Commander)
 admin.site.register(Deck)
+admin.site.register(Pod)
+admin.site.register(Game)
+admin.site.register(PodPlayer)
 
 # Register your models here.
