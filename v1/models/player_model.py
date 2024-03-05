4f6b48b0f95ccae7a71dfd70de81edbb20a3492e
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-03-04 18:28:13 -0600

    Add the statistics attributes to the player model for rendering

--- v1/models/player_model.py
+++ v1/models/player_model.py
@@ -5,6 +5,27 @@ class Player(models.Model):
     photo = models.ImageField(upload_to='images/', null=True, blank=True)
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=50)
+    total_wins = models.IntegerField(default=0)
+    total_games = models.IntegerField(default=0)
+    total_win_perc = models.FloatField(default=0)
+    total_kills = models.IntegerField(default=0)
+    kill_avg = models.FloatField(default=0)
+    games_as_arch = models.IntegerField(default=0)
+    wins_as_arch = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
+    
+    def calc_win_perc(self):
+        if self.total_games > 0:
+            self.total_win_perc = (self.total_wins / self.total_games) * 100
+        else:
+            self.total_win_perc = 0.0
+        self.save()
+
+    def calc_kill_avg(self):
+        if self.total_kills > 0:
+            self.kill_avg = (self.total_kills / self.total_games) * 100
+        else:
+            self.total_kills = 0
+        self.save()
