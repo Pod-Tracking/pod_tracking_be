85d095b3f716122066a7e953df9f923c2339796e
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-08-12 02:39:52 -0500

    Update the serializers to be more in line with the model changes

--- v1/serializers/commander_serializer.py
+++ v1/serializers/commander_serializer.py
@@ -4,4 +4,4 @@ from ..models.commander_model import Commander
 class CommanderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Commander
-        fields = ['id', 'name', 'colors', 'img', 'created_at', 'updated_at']
+        fields = ['id', 'deck', 'name', 'colors', 'photo', 'created_at', 'updated_at']
--- v1/serializers/deck_serializer.py
+++ v1/serializers/deck_serializer.py
@@ -4,4 +4,4 @@ from ..models.deck_model import Deck
 class DeckSerializer(serializers.ModelSerializer):
     class Meta:
         model = Deck
-        fields = ['id', 'player', 'cmdr', 'name', 'tcg', 'deck_type', 'colors', 'photo', 'total_wins', 'total_losses', 'created_date', 'updated_date']
+        fields = ['id', 'player', 'name', 'tcg', 'deck_type', 'colors', 'photo', 'total_wins', 'total_games', 'created_at', 'updated_at']
