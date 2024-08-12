71f1a4f0783598302b95ce8c1c2bed3a38d9c039
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-08-12 02:41:36 -0500

    Refactored the previously existing views to have better type-ing and simpilier SQL queries

--- v1/views/deck_views.py
+++ v1/views/deck_views.py
@@ -3,45 +3,47 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from ..serializers.deck_serializer import DeckSerializer
-# from ..serializers.player_serializer import PlayerSerializer
-# from ..serializers.commander_serializer import CommanderSerializer
 from ..models.deck_model import Deck
 from ..models.player_model import Player
-# from ..models.commander_model import Commander
+import pdb
+
 
 @api_view(['GET', 'POST'])
-def deck_list(request, player_id):
+def deck_list(request, player_id: int):
     try:
-        check_for_player = Player.objects.get(pk=player_id)
+        checked_player = Player.objects.get(pk=player_id)
     except Player.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
     
     if request.method == 'GET':
-        return get_deck_list(request, check_for_player)
+        return get_deck_list(checked_player)
     elif request.method == 'POST':
-        return create_deck(request)
+        return create_deck(request.data, player_id)
 
-def get_deck_list(request, check_for_player):
-    decks = Deck.objects.filter(player=check_for_player)
+def get_deck_list(checked_player: Player):
+    decks = Deck.objects.filter(player=checked_player)
     serializer = DeckSerializer(decks, many=True)
     return Response(serializer.data)
 
-def create_deck(request):
-    serializer = DeckSerializer(data=request.data)
+def create_deck(data: dict, player_id: int):
+    new_deck_data = {
+        "name": data["name"],
+        "player": player_id,
+        "tcg": data["tcg"],
+        "deck_type": data["deck_type"]
+    }
+
+    serializer = DeckSerializer(data=new_deck_data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
 
+
 @api_view(['GET', 'PUT', 'DELETE'])
-def deck_details(request, deck_id, player_id):
+def deck_details(request, deck_id: int, player_id: int):
     try:
-        player = Player.objects.get(pk=player_id)
-    except Player.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    try:
-        deck = Deck.objects.get(pk=deck_id)
+        deck = Deck.objects.get(pk=deck_id, player=player_id)
     except Deck.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
     
@@ -52,17 +54,17 @@ def deck_details(request, deck_id, player_id):
     elif request.method == 'DELETE':
         return delete_deck(deck)
 
-def get_deck_details(deck):
+def get_deck_details(deck: Deck):
     serializer = DeckSerializer(deck)
     return Response(serializer.data)
 
-def update_deck(deck, data):
+def update_deck(deck: Deck, data: dict):
     deck_data = DeckSerializer(deck, data=data, partial=True)
     if deck_data.is_valid():
         deck_data.save()
         return Response(deck_data.data)
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
-def delete_deck(deck):
+def delete_deck(deck: Deck):
     deck.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
