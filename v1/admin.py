960961d3a60536451d48305bd4f33ce3e5ae7679
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-08-15 04:34:33 -0500

    Update the admin.py file for the newer models that were added to the database

--- v1/admin.py
+++ v1/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models.commander_model import Commander
 from .models.player_model import Player
 from .models.deck_model import Deck
+from .models.commander_model import Commander
 from .models.pod_model import Pod
-from .models.pod_player_model import PodPlayer
 from .models.game_model import Game
+from .models.pod_player_model import PodPlayer
+from .models.game_player_model import GamePlayer
 
 admin.site.register(Player)
 admin.site.register(Commander)
@@ -12,5 +13,5 @@ admin.site.register(Deck)
 admin.site.register(Pod)
 admin.site.register(Game)
 admin.site.register(PodPlayer)
-
+admin.site.register(GamePlayer)
 # Register your models here.
