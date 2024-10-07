1431639f4553ed0ff7ccad25880b57ea23f1390a
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-10-07 01:16:48 -0500

    Create the validate_tcg_type and validate_deck_type utility functions for the project

--- v1/utils.py
+++ v1/utils.py
@@ -1,4 +1,7 @@
 from .enums.color_enum import MtgColor
+from .enums.deck_type_enum import DeckType
+from .enums.tcg_type_enum import TcgType
+
 
 def process_colors(color_list: list) -> str:
     color_values = []
@@ -8,3 +11,15 @@ def process_colors(color_list: list) -> str:
         else:
             raise ValueError(f"Invalid color value: {color}")
     return ', '.join(color_values)
+
+def validate_tcg_type(tcg_type: str) -> str:
+    if tcg_type in TcgType.__members__:
+        return tcg_type
+    else:
+        raise ValueError(f"Invalid TCG type: {tcg_type}")
+
+def validate_deck_type(deck_type: str) -> str:
+    if deck_type in DeckType.__members__:
+        return deck_type
+    else:
+        raise ValueError(f"Invalid Deck type: {deck_type}")
