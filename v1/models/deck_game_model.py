a6cb5378f97364eb2641aabcfb2150d6f6b334e8
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-10-13 17:39:34 -0500

    Remove the extra file from the project since it was not needed

--- v1/models/deck_game_model.py
+++
@@ -1,8 +0,0 @@
-# from django.db import models
-# from .game_model import Game
-# from .deck_model import Deck
-
-# class DeckGame(models.Model):
-#     deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
-#     game = models.ForeignKey(Game, on_delete=models.CASCADE)
-#     # turns_taken = models.IntegerField(default=0)
